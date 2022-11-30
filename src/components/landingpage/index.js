d1259cbe590caaf8f2d7a9e1f2c30e3fcc1b8c57
Gavin Giddings <Gavin>
2022-11-30 17:48:08 -0500

    Updated Carousel

--- src/components/landingpage/index.js
+++ src/components/landingpage/index.js
@@ -11,25 +11,25 @@ export default function Landingpage(){
     return (
         <div>
             <MDBCarousel>
-        <MDBCarouselItem
-          className="w-100 d-block"
-          itemId={1}
-          src="https://mdbootstrap.com/img/new/slides/041.jpg"
-          alt="..."
-        />
-        <MDBCarouselItem
-          className="w-100 d-block"
-          itemId={2}
-          src="https://mdbootstrap.com/img/new/slides/042.jpg"
-          alt="..."
-        />
-        <MDBCarouselItem
-          className="w-100 d-block"
-          itemId={3}
-          src="https://mdbootstrap.com/img/new/slides/043.jpg"
-          alt="..."
-        />
-      </MDBCarousel>
+              <MDBCarouselItem
+                className="w-100 d-block"
+                itemId={1}
+                src="https://gray-wdam-prod.cdn.arcpublishing.com/resizer/6PHBIGGn9iUqfMw6c5lAk33EZMg=/1200x675/smart/filters:quality(85)/cloudfront-us-east-1.images.arcpublishing.com/gray/FIVPU6JDKJGSVJSSW7AEIP3MEQ.png"
+                alt="..."
+              />
+              <MDBCarouselItem
+                className="w-100 d-block"
+                itemId={2}
+                src="https://mdbootstrap.com/img/new/slides/042.jpg"
+                alt="..."
+              />
+              <MDBCarouselItem
+                className="w-100 d-block"
+                itemId={3}
+                src="https://mdbootstrap.com/img/new/slides/043.jpg"
+                alt="..."
+              />
+            </MDBCarousel>
             <GalleryDiv>
                 
             </GalleryDiv>
