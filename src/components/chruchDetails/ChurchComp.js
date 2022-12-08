8bb9201ff95f2af7f752f91788da776422373010
gavingiddings1997@gmail.com <Gavin>
2022-12-08 17:17:45 -0500

    added fetch for church listings

--- src/components/chruchDetails/ChurchComp.js
+++ src/components/chruchDetails/ChurchComp.js
@@ -3,31 +3,65 @@ import { useNavigate } from "react-router-dom";
 import Context from "../../context/Context";
 
 const Details = (props) => {
-  const { setdoNate, doNate } = useContext(Context)
+  const { setdoNate, doNate ,userData } = useContext(Context);
+  const [submissionData , SetsubmissionData] = useState(null)
   // fetch call to backend server
+  useEffect(()=>{
   async function changeData() {
-    const data = await fetch("http://localhost:3100/listings/:id", {
-      method: "PATCH",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({ id: props.id })
-    }
-    ).then(response => response.json())
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
+    let raw = JSON.stringify({
+      id: props.id,
+      user_id: userData.id,
+      itemDescription: submissionData.description,
+      time: submissionData.time,
+    });
+
+    let requestOptions = {
+      method: "PATCH",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("localhost:3100/listings/church/1", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+
+      .catch((error) => console.log("error", error));
+    // const data = await fetch("http://localhost:3100/listings/:id", {
+    //   method: "PATCH",
+    //   headers: { "Content-type": "application/json" },
+    //   body: JSON.stringify({ id: props.id })
+    // }
+    // ).then(response => response.json())
   }
+
+  if(submissionData){
+    changeData()
+    
+  }
+},[submissionData])
   const navigate = useNavigate();
   const goToHomePage = () => {
-    navigate('/church');
-  } 
+    navigate("/church");
+  };
 
   const changeStatus = (e) => {
     e.preventDefault();
+    SetsubmissionData({
+      time: e.target[0].value,
+      description: e.target[1].value
+    })
     //invokes the function to fetch to the backend
-    changeData()
+
+    
     // updates the state and places the church donated to to the bottom of the page
-    setdoNate(doNate+1)
+    setdoNate(doNate + 1);
     // takes the user back to the hompage
-    goToHomePage()
-  }
+    goToHomePage();
+  };
 
   return (
     <div>
@@ -40,9 +74,7 @@ const Details = (props) => {
       />
       <h4>{props.location}</h4>
       <form onSubmit={changeStatus}>
-        <label for="start">
-          Enter the Date:
-        </label>
+        <label for="start">Enter the Date:</label>
         <input type="submit" value="Submit" />
       </form>
     </div>
