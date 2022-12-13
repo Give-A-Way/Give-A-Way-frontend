b61992f311050dae5d073477c84a0a553fb1bef5
gavingiddings1997@gmail.com <Gavin>
2022-12-13 09:29:28 -0500

    restyled cards and button

--- src/components/churches/Chruch_listing.js
+++ src/components/churches/Chruch_listing.js
@@ -1,55 +1,81 @@
 import { Link } from "react-router-dom";
 import { useContext, useState } from "react";
 import Context from "../../context/Context";
-import * as mdb from 'mdb-ui-kit';
+import * as mdb from "mdb-ui-kit";
+import styled from "@emotion/styled";
 import {
   MDBCard,
   MDBCardBody,
   MDBCardTitle,
   MDBCardText,
   MDBCardImage,
-  MDBBtn
-} from 'mdb-react-ui-kit';
+  MDBBtn,
+} from "mdb-react-ui-kit";
+
+
 
 const ListChurches = (props) => {
-  const { isUserLogedIn } = useContext(Context)
-  const [donateError, setDonateError] = useState("")
+  const { isUserLogedIn } = useContext(Context);
+  const [donateError, setDonateError] = useState("");
   return (
     <div>
-    
-
-
-      <MDBCard style={{ boxShadow:"rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px", width: "300px", height: "400px", margin: "10px 5px" }}>
-        <MDBCardImage style={{ width: "300px", height: "200px" }} src={props.churchImg} position='top' alt={props.churchImg} />
-        <MDBCardBody style={
-          
-          {display: "flex",
-              flexWrap: "wrap",
-              flexDirection: "column",
-              justifyContent: "space-between"}
-              }>
+      <MDBCard
+        style={{
+          boxShadow:
+            "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+          width: "300px",
+          height: "400px",
+          margin: "10px 5px",
+        }}
+      >
+        <MDBCardImage
+          style={{ width: "300px", height: "200px" }}
+          src={props.churchImg}
+          position="top"
+          alt={props.churchImg}
+        />
+        <MDBCardBody
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
           <div>
-          <MDBCardTitle>{props.title}</MDBCardTitle>
-          <MDBCardText>
-            {props.address}
-          </MDBCardText>
+            <MDBCardTitle>{props.title}</MDBCardTitle>
+            <MDBCardText>{props.address}</MDBCardText>
           </div>
-          {isUserLogedIn ? <button href='#'><Link to={`${props.linkID}`}>Donate</Link></button > :
-            <div >
-              <button  
+          {isUserLogedIn ? (
+            
+            <button style={{
+              backgroundColor: "#2ea44e",
+              borderRadius: "10px",
+              textDecoration: "none",
+            }} href="#">
+              <Link 
+              style={
+                {color: "black",
+                textDecoration: "none"}
+              }
+              to={`${props.linkID}`}>Donate</Link>
+            </button>
+          ) : (
+            <div>
+              <button
                 href="#"
                 onClick={(e) => {
-                e.preventDefault(); 
-                setDonateError("login first");
-                }}>Donate</button>
+                  e.preventDefault();
+                  setDonateError("login first");
+                }}
+              >
+                Donate
+              </button>
               <p style={{ margin: "5px 10px" }}>{donateError}</p>
             </div>
-          }
+          )}
         </MDBCardBody>
       </MDBCard>
-
-
-
 
       {/* {isUserLogedIn ? <button>
         <Link to={`${props.linkID}`}>Donate</Link>
@@ -64,4 +90,4 @@ const ListChurches = (props) => {
     </div>
   );
 };
-export default ListChurches
+export default ListChurches;
